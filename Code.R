de1bf3f745902a37719ec8009039b019b2f33cf0
Clara Nessi <clara.lewin@etu.unige.ch>
2021-10-20 16:06:39 +0200

    2nd table

--- Code.R
+++ Code.R
@@ -1,5 +1,7 @@
 ##Script for the project "European countries, their dietary habits and diabetes prevalence"
 
 library(readr)
+
 daily_caloric <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/daily-caloric.csv")
-View(daily_caloric)
+
+GDP <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/GDP.csv")
