9b26607169925a49c987baabd6b4b985c3b3981a
Clara Nessi <clara.lewin@etu.unige.ch>
2021-10-20 16:07:35 +0200

    m

--- Code.R
+++ Code.R
@@ -5,3 +5,4 @@ library(readr)
 daily_caloric <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/daily-caloric.csv")
 
 GDP <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/GDP.csv")
+ m
