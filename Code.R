a3a0fef3697ea63d0a929bacd5adcfc43f09c7c7
clara-ness <68824858+clara-ness@users.noreply.github.com>
2021-10-20 16:21:59 +0200

    m

--- Code.R
+++ Code.R
@@ -1,6 +1,7 @@
 ##Script for the project "European countries, their dietary habits and diabetes prevalence"
 
 library(readr)
+m 
 
 daily_caloric <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/daily-caloric.csv")
 
