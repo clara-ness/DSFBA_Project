ca59f4384f19be5fa3c0614ba4917bee0c45373d
Clara Nessi <clara.lewin@etu.unige.ch>
2021-10-20 16:02:40 +0200

    added 1st csv

--- Code.R
+++ Code.R
@@ -1,1 +1,5 @@
 ##Script for the project "European countries, their dietary habits and diabetes prevalence"
+
+library(readr)
+daily_caloric <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/daily-caloric.csv")
+View(daily_caloric)
