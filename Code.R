166d07758d6d157d7fc8c19a4bbc28c7441d0341
clara-ness <68824858+clara-ness@users.noreply.github.com>
2021-10-20 17:16:36 +0200

    change path

--- Code.R
+++ Code.R
@@ -1,10 +1,10 @@
 ##Script for the project "European countries, their dietary habits and diabetes prevalence"
 
 library(readr)
-m 
+ 
 
-daily_caloric <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/daily-caloric.csv")
+daily_caloric <- read_csv("daily-caloric.csv")
 
-GDP <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/GDP.csv")
+GDP <- read_csv("GDP.csv")
 
-Diabetes <- read_csv("C:/Users/Clara/Desktop/GSEM 3ème/Semestre de Printemps/Introduction to Data Science/Repo/DSFBA_Project/Diabetes.csv")
+Diabetes <- read_csv("Diabetes.csv")
